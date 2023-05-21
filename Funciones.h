049334503847352ee1dcee84cd549d0598747e33
MIQUELZAVALA <99816730+MIQUELZAVALA@users.noreply.github.com>
2023-05-21 21:53:21 +0200

    Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -21,4 +21,39 @@ void printFuelGas();
 void printTurbinaGas();
 void printTurbinaVapor();
 void printCicloCombinado();
-
+void printMayorValorDiaHidraulica();//no tiene sentido esta funcion
+void printMayorValorMesHidraulica();
+void printPeorValorDiaHidraulica();//no tiene sentido esta funcion
+void printPeorValorMesHidraulica();
+void printMediaAnualHidraulica2021();
+void printMediaAnualHidraulica2022();
+void printMayorValorDiaHidroeolica();//no tiene sentido esta funcion
+void printMayorValorMesHidroeolica();
+void printPeorValorDiaHidroeolica();//no tiene sentido esta funcion
+void printPeorValorMesHidroeolica();
+void printMediaAnualHidroeolica2021();
+void printMediaAnualHidroeolica2022();
+void printMayorValorDiaEolica();//no tiene sentido esta funcion
+void printMayorValorMesEolica();
+void printPeorValorDiaEolica();//no tiene sentido esta funcion
+void printPeorValorMesEolica();
+void printMediaAnualEolica2021();
+void printMediaAnualEolica2022();
+void printMayorValorDiaFotovoltaica();//no tiene sentido esta funcion
+void printMayorValorMesFotovoltaica();
+void printPeorValorDiaFotovoltaica();//no tiene sentido esta funcion
+void printPeorValorMesFotovoltaica();
+void printMediaAnualFotovoltaica2021();
+void printMediaAnualFotovoltaica2022();
+void printMayorValorDiaSolarTermica();//no tiene sentido esta funcion
+void printMayorValorMesSolarTermica();
+void printPeorValorDiaSolarTermica();//no tiene sentido esta funcion
+void printPeorValorMesSolarTermica();
+void printMediaAnualSolarTermica2021();
+void printMediaAnualSolarTermica2022();
+void printMayorValorDiaOtrasRenovables();//no tiene sentido esta funcion
+void printMayorValorMesOtrasRenovables();
+void printPeorValorDiaOtrasRenovables();//no tiene sentido esta funcion
+void printPeorValorMesOtrasRenovables();
+void printMediaAnualOtrasRenovables2021();
+void printMediaAnualOtrasRenovables2022();
