4ea1a6b541d514d0cc7a41dd6aa78fb33354ab87
Pablo2h10 <98923506+Pablo2h10@users.noreply.github.com>
2023-05-25 19:17:24 +0200

    Update Funciones.h
    Cambio Estructural y de Funciones Definitivas

--- Programa Final/Funciones.h
+++ Programa Final/Funciones.h
@@ -1,121 +1,173 @@
 void menuPrincipal();
+
 void printEnergias();
 void printTiposEnergias();
+
 void printTiposEnergiasNoRenovables();
 void printCombustiblesFosiles();
+
 void printNuclear();
+void printMayorValorMesNuclear2021();
+void printMayorValorMesNuclear2022();
+void printMayorValorMesNuclear2021_2022();
+void printMenorValorMesNuclear2021();
+void printMenorValorMesNuclear2022();
+void printMenorValorMesNuclear2021_2022();
+void printMediaAnualNuclear2021();
+void printMediaAnualNuclear2022();
+
 void printMotoresDiesel();
-void printTiposEnergiasRenovables();
-void printHidraulica();
-void printHidroeolica();
-void printEolica();
-void printSolarFotovoltaica();
-void printSolarTermica();
-void printOtrasEnergiasRenovables();
-void printGeneracionTotal();
-void printResiduos();
-void printResiduosRenovables();
-void printResiduosNoRenovables();
-void printMayorValorDiaResiduosRenovables();
-void printMayorValorMesResiduosRenovables();
-void printPeorValorDiaResiduosRenovables();
-void printPeorValorMesResiduosRenovables();
-void printMediaAnualResiduos2021Renovables();
-void printMediaAnualResiduos2022Renovables();
-void printMayorValorDiaResiduosNoRenovables();
-void printMayorValorMesResiduosNoRenovables();
-void printPeorValorDiaResiduosNoRenovables();
-void printPeorValorMesResiduosNoRenovables();
-void printMediaAnualResiduos2021NoRenovables();
-void printMediaAnualResiduos2022NoRenovables();
-void printGeneracionTotal();
-void printCombustiblesFosiles();
-void printCarbon();
-void printFuelGas();
-void printTurbinaGas();
-void printTurbinaVapor();
-void printCicloCombinado();
-void printMayorValorDiaHidraulica();//no tiene sentido esta funcion
-void printMayorValorMesHidraulica();
-void printPeorValorDiaHidraulica();//no tiene sentido esta funcion
-void printPeorValorMesHidraulica();
-void printMediaAnualHidraulica2021();
-void printMediaAnualHidraulica2022();
-void printMayorValorDiaHidroeolica();//no tiene sentido esta funcion
-void printMayorValorMesHidroeolica();
-void printPeorValorDiaHidroeolica();//no tiene sentido esta funcion
-void printPeorValorMesHidroeolica();
-void printMediaAnualHidroeolica2021();
-void printMediaAnualHidroeolica2022();
-void printMayorValorDiaEolica();//no tiene sentido esta funcion
-void printMayorValorMesEolica();
-void printPeorValorDiaEolica();//no tiene sentido esta funcion
-void printPeorValorMesEolica();
-void printMediaAnualEolica2021();
-void printMediaAnualEolica2022();
-void printMayorValorDiaFotovoltaica();//no tiene sentido esta funcion
-void printMayorValorMesFotovoltaica();
-void printPeorValorDiaFotovoltaica();//no tiene sentido esta funcion
-void printPeorValorMesFotovoltaica();
-void printMediaAnualFotovoltaica2021();
-void printMediaAnualFotovoltaica2022();
-void printMayorValorDiaSolarTermica();//no tiene sentido esta funcion
-void printMayorValorMesSolarTermica();
-void printPeorValorDiaSolarTermica();//no tiene sentido esta funcion
-void printPeorValorMesSolarTermica();
-void printMediaAnualSolarTermica2021();
-void printMediaAnualSolarTermica2022();
-void printMayorValorDiaOtrasRenovables();//no tiene sentido esta funcion
-void printMayorValorMesOtrasRenovables();
-void printPeorValorDiaOtrasRenovables();//no tiene sentido esta funcion
-void printPeorValorMesOtrasRenovables();
-void printMediaAnualOtrasRenovables2021();
-void printMediaAnualOtrasRenovables2022();
-void printMayorValorDiaCarbon();//no tiene sentido esta funcion
-void printMayorValorMesCarbon();
-void printPeorValorDiaCarbon();//no tiene sentido esta funcion
-void printPeorValorMesCarbon();
-void printMediaAnualCarbon2021();
-void printMediaAnualCarbon2022();
-void printMayorValorDiaFuelGas();//no tiene sentido esta funcion
-void printMayorValorMesFuelGas();
-void printPeorValorDiaFuelGas();//no tiene sentido esta funcion
-void printPeorValorMesFuelGas();
-void printMediaAnualFuelGas2021();
-void printMediaAnualFuelGas2022();
-void printMayorValorDiaTurbinaGas();//no tiene sentido esta funcion
-void printMayorValorMesTurbinaGas();
-void printPeorValorDiaTurbinaGas();//no tiene sentido esta funcion
-void printPeorValorMesTurbinaGas();
-void printMediaAnualTurbinaGas2021();
-void printMediaAnualTurbinaGas2022();
-void printMayorValorDiaTurbinaVapor();//no tiene sentido esta funcion
-void printMayorValorMesTurbinaVapor();
-void printPeorValorDiaTurbinaVapor();//no tiene sentido esta funcion
-void printPeorValorMesTurbinaVapor();
-void printMediaAnualTurbinaVapor2021();
-void printMediaAnualTurbinaVapor2022();
-void printMayorValorDiaCicloCombinado();//no tiene sentido esta funcion
-void printMayorValorMesCicloCombinado();
-void printPeorValorDiaCicloCombinado();//no tiene sentido esta funcion
-void printPeorValorMesCicloCombinado();
-void printMediaAnualCicloCombinado2021();
-void printMediaAnualCicloCombinado2022();
-void printMayorValorDiaMotoresDiesel();//no tiene sentido esta funcion
-void printMayorValorMesMotoresDiesel();
-void printPeorValorDiaMotoresDiesel();//no tiene sentido esta funcion
-void printPeorValorMesMotoresDiesel();
+void printMayorValorMesMotoresDiesel2021();
+void printMayorValorMesMotoresDiesel2022();
+void printMayorValorMesMotoresDiesel2021_2022();
+void printMenorValorMesMotoresDiesel2021();
+void printMenorValorMesMotoresDiesel2022();
+void printMenorValorMesMotoresDiesel2021_2022();
 void printMediaAnualMotoresDiesel2021();
 void printMediaAnualMotoresDiesel2022();
-void printMayorValorDiaResiduos();//no tiene sentido esta funcion
-void printMayorValorMesResiduos();
-void printPeorValorDiaResiduos();//no tiene sentido esta funcion
-void printPeorValorMesResiduos();
-void printMediaAnualResiduos2021();
-void printMediaAnualResiduos2022();
-void printMayorValorDiaGeneracionTotal();//no tiene sentido esta funcion
-void printMayorValorMesGeneracionTotal();
-void printPeorValorDiaGeneracionTotal();//no tiene sentido esta funcion
-void printPeorValorMesGeneracionTotal();
+
+void printTiposEnergiasRenovables();
+
+void printHidraulica();
+void printMayorValorMesHidraulica2021();
+void printMayorValorMesHidraulica2022();
+void printMayorValorMesHidraulica2021_2022();
+void printMenorValorMesHidraulica2021();
+void printMenorValorMesHidraulica2022();
+void printMenorValorMesHidraulica2021_2022();
+void printMediaAnualHidraulica2021();
+void printMediaAnualHidraulica2022();
+
+void printHidroeolica();
+void printMayorValorMesHidroeolica2021();
+void printMayorValorMesHidroeolica2022();
+void printMayorValorMesHidroeolica2021_2022();
+void printMenorValorMesHidroeolica2021();
+void printMenorValorMesHidroeolica2022();
+void printMenorValorMesHidroeolica2021_2022();
+void printMediaAnualHidroeolica2021();
+void printMediaAnualHidroeolica2022();
+
+void printEolica();
+void printMayorValorMesEolica2021();
+void printMayorValorMesEolica2022();
+void printMayorValorMesEolica2021_2022();
+void printMenorValorMesEolica2021();
+void printMenorValorMesEolica2022();
+void printMenorValorMesEolica2021_2022();
+void printMediaAnualEolica2021();
+void printMediaAnualEolica2022();
+
+void printSolarFotovoltaica();
+void printMayorValorMesSolarFotovoltaica2021();
+void printMayorValorMesSolarFotovoltaica2022();
+void printMayorValorMesSolarFotovoltaica2021_2022();
+void printMenorValorMesSolarFotovoltaica2021();
+void printMenorValorMesSolarFotovoltaica2022();
+void printMenorValorMesSolarFotovoltaica2021_2022();
+void printMediaAnualSolarFotovoltaica2021();
+void printMediaAnualSolarFotovoltaica2022();
+
+void printSolarTermica();
+void printMayorValorMesSolarTermica2021();
+void printMayorValorMesSolarTermica2022();
+void printMayorValorMesSolarTermica2021_2022();
+void printMenorValorMesSolarTermica2021();
+void printMenorValorMesSolarTermica2022();
+void printMenorValorMesSolarTermica2021_2022();
+void printMediaAnualSolarTermica2021();
+void printMediaAnualSolarTermica2022();
+
+void printOtrasEnergiasRenovables();
+void printMayorValorMesOtrasEnergiasRenovables2021();
+void printMayorValorMesOtrasEnergiasRenovables2022();
+void printMayorValorMesOtrasEnergiasRenovables2021_2022();
+void printMenorValorMesOtrasEnergiasRenovables2021();
+void printMenorValorMesOtrasEnergiasRenovables2022();
+void printMenorValorMesOtrasEnergiasRenovables2021_2022();
+void printMediaAnualOtrasEnergiasRenovables2021();
+void printMediaAnualOtrasEnergiasRenovables2022();
+
+void printResiduos();
+void printResiduosRenovables();
+
+void printResiduosNoRenovables();
+void printMayorValorMesResiduosNoRenovables2021();
+void printMayorValorMesResiduosNoRenovables2022();
+void printMayorValorMesResiduosNoRenovables2021_2022();
+void printMenorValorMesResiduosNoRenovables2021();
+void printMenorValorMesResiduosNoRenovables2022();
+void printMenorValorMesResiduosNoRenovables2021_2022();
+void printMediaAnualResiduosNoRenovables2021();
+void printMediaAnualResiduosNoRenovables2022();
+
+void printResiduosNoRenovables();
+void printMayorValorMesResiduosNoRenovables2021();
+void printMayorValorMesResiduosNoRenovables2022();
+void printMayorValorMesResiduosNoRenovables2021_2022();
+void printMenorValorMesResiduosNoRenovables2021();
+void printMenorValorMesResiduosNoRenovables2022();
+void printMenorValorMesResiduosNoRenovables2021_2022();
+void printMediaAnualResiduosNoRenovables2021();
+void printMediaAnualResiduosNoRenovables2022();
+
+void printGeneracionTotal();
+void printMayorValorMesGeneracionTotal2021();
+void printMayorValorMesGeneracionTotal2022();
+void printMayorValorMesGeneracionTotal2021_2022();
+void printMenorValorMesGeneracionTotal2021();
+void printMenorValorMesGeneracionTotal2022();
+void printMenorValorMesGeneracionTotal2021_2022();
 void printMediaAnualGeneracionTotal2021();
 void printMediaAnualGeneracionTotal2022();
+
+void printCombustiblesFosiles();
+void printCarbon();
+void printMayorValorMesCarbon2021();
+void printMayorValorMesCarbon2022();
+void printMayorValorMesCarbon2021_2022();
+void printMenorValorMesCarbon2021();
+void printMenorValorMesCarbon2022();
+void printMenorValorMesCarbon2021_2022();
+void printMediaAnualCarbon2021();
+void printMediaAnualCarbon2022();
+
+void printFuelGas();
+void printMayorValorMesFuelGas2021();
+void printMayorValorMesFuelGas2022();
+void printMayorValorMesFuelGas2021_2022();
+void printMenorValorMesFuelGas2021();
+void printMenorValorMesFuelGas2022();
+void printMenorValorMesFuelGas2021_2022();
+void printMediaAnualFuelGas2021();
+void printMediaAnualFuelGas2022();
+
+void printTurbinaGas();
+void printMayorValorMesTurbinaGas2021();
+void printMayorValorMesTurbinaGas2022();
+void printMayorValorMesTurbinaGas2021_2022();
+void printMenorValorMesTurbinaGas2021();
+void printMenorValorMesTurbinaGas2022();
+void printMenorValorMesTurbinaGas2021_2022();
+void printMediaAnualTurbinaGas2021();
+void printMediaAnualTurbinaGas2022();
+
+void printTurbinaVapor();
+void printMayorValorMesTurbinaVapor2021();
+void printMayorValorMesTurbinaVapor2022();
+void printMayorValorMesTurbinaVapor2021_2022();
+void printMenorValorMesTurbinaVapor2021();
+void printMenorValorMesTurbinaVapor2022();
+void printMenorValorMesTurbinaVapor2021_2022();
+void printMediaAnualTurbinaVapor2021();
+void printMediaAnualTurbinaVapor2022();
+
+void printCicloCombinado();
+void printMayorValorMesCicloCombinado2021();
+void printMayorValorMesCicloCombinado2022();
+void printMayorValorMesCicloCombinado2021_2022();
+void printMenorValorMesCicloCombinado2021();
+void printMenorValorMesCicloCombinado2022();
+void printMenorValorMesCicloCombinado2021_2022();
+void printMediaAnualCicloCombinado2021();
+void printMediaAnualCicloCombinado2022();
